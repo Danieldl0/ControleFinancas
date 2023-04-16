28812f5ba42fc19d13f8f5893f00b2b9845c22c0
Daniel Oliveira <daniel.d.oliveira20@gmail.com>
2023-04-16 13:29:56 -0300

    criação do login + logout

--- usuario/forms.py
+++ usuario/forms.py
@@ -1,27 +1,36 @@
 from django.forms import ModelForm, CharField, EmailField, ValidationError, PasswordInput
 from django.contrib.auth.models import User
-from django.contrib.auth.password_validation import validate_password
 
 
 class UserForm(ModelForm):
-    username = CharField(help_text=None)
-    first_name = CharField(required=True)
-    last_name = CharField(required=True)
+    # modificando os parametros dos atributos da classe User
+    username = CharField(help_text=None, label="Usuario")
+    first_name = CharField(required=True, label="Nome")
+    last_name = CharField(required=True, label="Sobrenome")
     email = EmailField(required=True)
-    password = CharField(required = True, widget=PasswordInput(), validators=[validate_password])
-    password2 = CharField(max_length=128, required=True, label="Confirmar senha", widget=PasswordInput())
+    password2 = CharField(max_length=128, required=True, widget=PasswordInput(), label="Confirmar senha")
 
+    
     class Meta:
         model = User
         fields = ["username","first_name", "last_name", "email", "password"]
 
 
+    ''' sobrepondo metodo clean para validar 
+        que a senha é a confirmar senha dela são iguais. '''
     def clean(self):
         cleaned_data = super(UserForm, self).clean()
         password = cleaned_data.get("password")
         password2 = cleaned_data.get("password2")
-
+        
         if password != password2:
             raise ValidationError(
                 "As senhas devem ser iguais"
             )
+        
+
+    # funcao para colocar o atributo class = "form-control" do boostrap em todos os campos do fomulario
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
--- usuario/views.py
+++ usuario/views.py
@@ -1,26 +1,49 @@
 from django.shortcuts import render, redirect
 from .forms import UserForm
 from django.urls import reverse
+from django.contrib import auth
+from django.contrib import messages
+from django.contrib.messages import constants
 
-# Create your views here.
+
 
 def login(request, template_name="login.html"):
     if request.method == "GET":
         return render(request, template_name)
     if request.method == "POST":
         
+        # pegando os dados do fomurlario. username e senha
         username = request.POST.get('username')
-        password = request.POST.get('senha')
+        password = request.POST.get('password')
 
-        
+        # autenticando o usuario com os dados de login
+        user = auth.authenticate(username=username, password=password)
 
+        # verificando se existe o usuario, caso não retorna uma mensagem de erro
+        if not user:
+            messages.add_message(request, constants.ERROR, 'Username ou senha inválidos')
+            return redirect(reverse('login'))
+
+        # Logar o usuario. criando uma seção para o userio autenticado.
+        auth.login(request, user)
         return render(request, template_name)
 
 
-def cadastro(request, template_name="cadastro.html"):
-    form = UserForm(request.POST or None)
+def logout(request):
+    auth.logout(request)
+    return redirect(reverse('login'))
 
+
+def cadastro(request, template_name="cadastro.html"):
+    #criando uma instancia de UserForm
+    form = UserForm(request.POST or None)
+    
+
+    #verificando se os dados são validos
     if form.is_valid():
-        form.save()
+        # criando um novo usuario com os dados do formulario
+        user = form.save() 
+        user.set_password(user.password) # salvando a senha como hash no banco de dados
+        user.save()
         return redirect(reverse('login'))
     return render(request, template_name, {"form": form})
